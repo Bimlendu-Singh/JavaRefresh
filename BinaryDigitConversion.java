ef4747f75aaf16a8249030b89bb0aacf8e7e2a8b
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-22 16:04:16 +0530

    writing binary digit conversion code

---
+++ BinaryDigitConversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BinaryDigitConversion {
+    public static void main(String[] args) {
+        
+        int i, num ;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number to get it's binary digit: ");
+        num = sc.nextInt();
+
+        System.out.println(num);
+    }
+}
