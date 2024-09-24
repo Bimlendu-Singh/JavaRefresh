4577ba67362944693b81a9fb0676d9973cfcba0d
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-24 18:41:33 +0530

    Refreshing data types

--- DataTypes.java
+++ DataTypes.java
@@ -2,7 +2,7 @@ public class DataTypes {
     public static void main(String[] args) {
         //Let's see all the data types here!
 
-        //..................... Boolean Example Starts...............
+//..................... Boolean Example Starts...............
         boolean man=true;
         boolean woman=false;
 
@@ -32,6 +32,20 @@ public class DataTypes {
         System.out.println("No");
         else
         System.out.println("Helloluiah");
+//..................... Boolean Example Starts...............
+
+        int mufti_Tshirt1Price =  6000;
+        long mTP = mufti_Tshirt1Price;  //Here implicit type casting is happening
+
+        System.out.println("This price is of int : "+ mufti_Tshirt1Price);
+        System.out.println("long price: " + mTP);
+
+        long quta = 44444444445999l; //We have to add lorL after number otherwise it will take it as int not long 
+        // int Quta = quta; this is wrong
+        int Quta = (int)quta; //Here, when we inserting long into int we have to type cast it like this (int) , otherwise error will come
+
+        System.out.println("Long number : " + quta);
+        System.out.println("int number : " + Quta);
 
     }
 }
