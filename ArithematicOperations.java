324f22fa74f604eae2d6bb73e606034a19dbf86e
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-25 15:56:48 +0530

    trying to understand if else block with code

--- ArithematicOperations.java
+++ ArithematicOperations.java
@@ -10,5 +10,21 @@ public class ArithematicOperations {
         System.out.println(a+d);
         System.out.println(a*d);
         System.out.println(a-d);
+
+     
+        if(a==b){  //We can't compare number directy in if bracket 
+            System.out.println("a equals b");
+        }
+        else{
+            System.out.println(" a not equals b");
+        }
+
+        boolean checkif  = false; 
+        if(checkif){  //We can't put 0 or 1 in if bracket like in java if(0)is wrong considered also we can't write true or false in bracket
+            System.out.println("if block running");
+        }
+        else{
+            System.out.println("else block running");
+        }
     }
 }
