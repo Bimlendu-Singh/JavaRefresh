a243f0b7618946e1caf4ef953f7144334c44deb5
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-25 20:21:46 +0530

    practiced nested if ele

--- ArithematicOperations.java
+++ ArithematicOperations.java
@@ -26,5 +26,67 @@ public class ArithematicOperations {
         else{
             System.out.println("else block running");
         }
+
+        int raniAge = 18;
+        int bheemAge = 24;
+
+        if(raniAge>18)
+        System.out.println("rani is eligible for vote");
+        else if(bheemAge>18)
+        System.out.println("Bheem is elgible for vote");
+        else
+        System.out.println("No one is elgible for vote");
+
+        int mineA=12, mineB = 19, mineC=4;
+
+        if(mineA>mineB)
+        {
+            if (mineA>mineC) {
+                System.out.println(mineA+" is greater");
+            }
+            else{
+                System.out.println("mineC is greater: "+mineC);
+            }
+        }else if(mineB>mineC){
+            if (mineB>mineA) {
+                System.out.println(mineB+" is greater");
+            }
+            else{
+                System.out.println("mineA is greater: "+mineA);
+            }
+        }else if(mineC>mineA){
+            if (mineC>mineB) {
+                System.out.println(mineC+" is greater");
+            }
+            else{
+                System.out.println("mineB is greater: "+mineB);
+            }
+        }
+        else{
+            System.out.println("All are equal hero!");
+        }
+
+        // int minA=72, minB = 72, minC=42;
+        // Below seems good but for above value it will show all are equal
+        // if(minA>minB)
+        // {
+        //     if (minA>minC) {
+        //         System.out.println(minA+" is greater");
+        //     }
+        //     else{
+        //         System.out.println("minC is greater: "+minC);
+        //     }
+        // }else if(minB>minA){
+        //     if (minB>minA) {
+        //         System.out.println(minB+" is greater");
+        //     }
+        //     else{
+        //         System.out.println("minA is greater: "+minA);
+        //     }
+        // }else{
+        //     System.out.println("All are equal hero!");
+        // }
+
+
     }
 }
