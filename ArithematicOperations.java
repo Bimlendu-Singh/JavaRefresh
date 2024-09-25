ae0d943c6fed64ccb9987a8f0de8e6a56a9dc8e4
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-25 15:45:03 +0530

    arithmetic

---
+++ ArithematicOperations.java
@@ -0,0 +1,14 @@
+public class ArithematicOperations {
+ 
+    public static void main(String[] args) {
+        int a=9,b=3,c=4;
+        float d=5;
+
+        System.out.println(a/b);
+        System.out.println(a/c);
+        System.out.println(a/d);
+        System.out.println(a+d);
+        System.out.println(a*d);
+        System.out.println(a-d);
+    }
+}
