6b80654b44cfb657565501f4c78c62c721c219a2
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-29 14:33:30 +0530

    java ternary operator qn updated

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class TernaryOperator {
     public static void main(String[] args) {
-        int age =18;
+        int age ;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your age: ");
+        age=sc.nextInt();
+        sc.close();
+
         String vote = (age>=18?"Can Vote":"Can't Vote");
         System.out.println("This person "+vote);
     }
