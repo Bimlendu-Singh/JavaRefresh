22f3384813409086f681631d97bc036a71df10f0
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-29 14:26:19 +0530

    java ternary operator qn

---
+++ TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int age =18;
+        String vote = (age>=18?"Can Vote":"Can't Vote");
+        System.out.println("This person "+vote);
+    }
+}
