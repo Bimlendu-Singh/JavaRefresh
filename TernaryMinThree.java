da41b7ffdf79b0eb0dd26af369cfff9edf8f1c09
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-09-30 17:42:32 +0530

    ternary minimung among three solved good question

--- TernaryMinThree.java
+++ TernaryMinThree.java
@@ -12,7 +12,13 @@ public class TernaryMinThree {
         c = sc.nextFloat();
         sc.close();
 
-        
+        //float result = (a<=b&&a<=c)?a:(b<=a&&b<=c)?b:(c<=a&&c<=b)?c:0.0f;// Here i can't write string with float like, "All are equal"
+        float result = (a<=b&&a<=c)?a:(b<=a&&b<=c)?b:c;
+
+        if(a==b&&b==c)
+        System.out.println("All numbers are equal :"+a);
+        else
+        System.out.println("Minimum among three is : "+result);
 
 
     }
