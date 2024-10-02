76837a12b2f2c39ed205cc66235e4ea83dbfeed9
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-02 23:34:13 +0530

    Problem in guessing word question

--- WhileNot.java
+++ WhileNot.java
@@ -3,5 +3,30 @@ import java.util.Scanner;
 public class WhileNot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter word to guess!");
+        String word = sc.nextLine();
+        
+
+        String guess="A";
+
+        while (guess!=word ) {
+            System.out.println("Guess the word: ");
+            guess = sc.nextLine();
+
+            if(guess==word)
+            {
+                System.out.println("Congratulation, You WON!");
+                break;
+            }
+            else if (guess=="I Loose") {
+                System.out.println("It's okay!, Correct word is : "+word);
+                break;
+            }
+            else
+               {System.out.println("Try Again!, or Type I Loose");} 
+        }
+        sc.close();
+
     }
 }
