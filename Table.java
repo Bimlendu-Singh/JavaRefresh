ffed569304b999f6fef0f41166806a15fdfb1ba5
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-04 15:03:09 +0530

    table program making using loop

---
+++ Table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number for it's table :");
+        int table = sc.nextInt();
+        sc.close();
+
+        
+
+    }
+}
