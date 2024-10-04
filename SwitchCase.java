32f5af88ee59f9d1fa151f2580158807837caf0a
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-04 14:16:29 +0530

    Week program using switch case

---
+++ SwitchCase.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+
+        System.out.println("Enter the days of the week: ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("It's Monday");
+                break;
+        
+            case 2:
+                System.out.println("It's Tuesday");
+                break;
+        
+            case 3:
+                System.out.println("It's Wednesday");
+                break;
+        
+            case 4:
+                System.out.println("It's Thursday");
+                break;
+        
+            case 5:
+                System.out.println("It's Friday");
+                break;
+        
+            case 6:
+                System.out.println("It's Saturday");
+                break;
+        
+            case 7:
+                System.out.println("It's Sunday");
+                break;
+        
+            default: System.out.println("Enter the right date!");
+                break;
+        }
+    }
+}
