51082946457d1c6cf6dc7ba5466432ad224c666d
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-07 17:35:30 +0530

    array marks question solved

---
+++ ArrayMarks.java
@@ -0,0 +1,9 @@
+public class ArrayMarks {
+    public static void main(String[] args) {
+        String name[]={"ab","abc","abd","abe","abf"};
+
+        for(int i=0;i<name.length;i++){
+            System.out.println(name[i]);
+        }
+    }
+}
