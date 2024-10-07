deca709c93e52d6c4f48d7f4391826f4f971e872
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-07 18:19:44 +0530

    name percentage code done

--- ArrayNameAndPercentage.java
+++ ArrayNameAndPercentage.java
@@ -7,14 +7,26 @@ public class ArrayNameAndPercentage {
         System.out.println("Enter the number of Students: ");
         int no_Of_Students = sc.nextInt();
 
+        // Consuming the leftover newline character after nextInt()
+        sc.nextLine();
+
         String[] studName = new String[no_Of_Students];
         float[] percentage = new float[no_Of_Students];
 
         for (int i = 0; i < no_Of_Students; i++) {
-            System.out.println((i+1)+".) Enter Student Name: ");
+            System.out.println((i+1)+") Enter Student Name: ");
             studName[i]=sc.nextLine();
             System.out.println("Enter their percentage : ");
             percentage[i]=sc.nextFloat();
+            // Consuming the leftover newline character after nextInt()
+            sc.nextLine();
         }
+
+        for (int i = 0; i < studName.length; i++) {
+            System.out.println((i+1)+") "+studName[i]+"  "+percentage[i]);
+        }
+
+        sc.close();
+        
     }
 }
