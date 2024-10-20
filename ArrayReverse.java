00664edf740f3a9eee216b3aedac0a2bfd98ce79
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-20 20:03:29 +0530

    Making java array reverse program update

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -1,6 +1,24 @@
 public class ArrayReverse {
     public static void main(String[] args) {
         int num[] = {10,20,30,40,50,80};
+        int i,j,k,temp;
+
+        j = num.length;
+        k=0;
+
+        System.out.println("Array before sorting: ");
+        for (int l = 0; l < num.length; l++) {
+            System.out.print(" "+num[l]);
+        }
+
+        for(i=0;i<=(num.length/2);i++)
+        {
+            temp=num[j];
+            num[j]=num[k];
+            num[k]=temp;
+            j--;
+            k++;
+        }
 
         System.out.println(num.length);
     }
