e6ed00eacd54b764e0a165a895ddef02d4e4238f
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-29 16:31:00 +0530

    Defining row and column in 2d array

--- Array2d.java
+++ Array2d.java
@@ -3,6 +3,17 @@ public class Array2d {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        //Taking input in 2d-array
+        int[][] marks = new int[2][3];
+        /*It will be like this
+          column1&2and3
+          row0[10 20 30]
+          row1[10 20 30]
+         */
+
+        System.out.println("Enter marks of students : ");
+        for (int i = 0; i < marks.length; i++) {
+            
+        }
     }
 }
