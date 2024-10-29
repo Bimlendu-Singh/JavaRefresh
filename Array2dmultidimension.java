936946e2c6a97d62ed83b71626f8fc89c45f5f77
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-10-29 19:36:48 +0530

    multidimensional array program

--- Array2dmultidimension.java
+++ Array2dmultidimension.java
@@ -2,5 +2,14 @@ import java.util.Scanner;
 public class Array2dmultidimension {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int[][] salary = new int[3][];
+        salary[0]= new int[2];
+        salary[1]= new int[3];
+        salary[2]= new int[4];
+
+        System.out.println(salary[0].length);
+        System.out.println(salary[1].length);
+        System.out.println(salary[2].length);
     }
 }
