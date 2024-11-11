e2b6eb01d9274651ceb70a16330e1f9d3267167c
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-11-11 16:44:20 +0530

    Creating inner ArrayList and assigned it values

--- ArrayList2D.java
+++ ArrayList2D.java
@@ -2,6 +2,24 @@ import java.util.ArrayList;
 
 public class ArrayList2D {
     public static void main(String[] args) {
+        // Creating Outer ArrayList
         ArrayList<ArrayList<Integer>> studentScores = new ArrayList<>();
+        // Making inner ArrayList
+        ArrayList<Integer> student1Scores = new ArrayList<>();
+        student1Scores.add(30);
+        student1Scores.add(50);
+        student1Scores.add(60);
+
+        ArrayList<Integer> student2Scores = new ArrayList<>();
+        student2Scores.add(38);
+        student2Scores.add(55);
+        student2Scores.add(70);
+
+        ArrayList<Integer> student3Scores = new ArrayList<>();
+        student3Scores.add(56);
+        student3Scores.add(79);
+        student3Scores.add(69);
+
+
     }
 }
