7d37b8fed70cf6b040936ef9f0b20c12b6fe7e23
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-11-11 16:40:07 +0530

    Creating outer ArrayList first

--- ArrayList2D.java
+++ ArrayList2D.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+
 public class ArrayList2D {
-    
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> studentScores = new ArrayList<>();
+    }
 }
