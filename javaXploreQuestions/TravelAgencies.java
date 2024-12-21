2bf819f92c9758fdf18ab5919ddab89349ffbd82
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-12-21 15:59:41 +0530

    Main method made in main class and made array object of TravelAgencies class

---
+++ javaXploreQuestions/TravelAgencies.java
@@ -0,0 +1,71 @@
+public class TravelAgencies {
+
+    // Declaring the class variables
+    int regNo;
+    String agencyName;
+    String packageType;
+    int price;
+    boolean FlightFacility;
+
+    // Constructor with parameters
+    public TravelAgencies(int regNo, String agencyName, String packageType, int price, boolean FlightFacility) {
+        this.regNo=regNo;
+        this.agencyName=agencyName;
+        this.packageType=packageType;
+        this.price=price;
+        this.FlightFacility=FlightFacility;
+    }
+
+    //Making setters for each variables
+    public void setRegNo(int regNo)
+    {
+        this.regNo=regNo;
+    }
+
+    public void setAgencyName(String agencyName)
+    {
+        this.agencyName=agencyName;
+    }
+
+    public void setPackageType(String packageType)
+    {
+        this.packageType=packageType;
+    }
+
+    public void setPrice(int price)
+    {
+        this.price=price;
+    }
+
+    public void setFlightFacility(boolean FlightFacility)
+    {
+        this.FlightFacility=FlightFacility;
+    }
+
+    //Making getters for each variables
+    public int getRegNo()
+    {
+        return regNo;
+    }
+
+    public String getAgencyName()
+    {
+        return agencyName;
+    }
+
+    public String getPackageType()
+    {
+        return packageType;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public boolean getFlightFacility()
+    {
+        return FlightFacility;
+    }
+
+}
--- javaXploreQuestions/javaQn1TravelAgencies.java
+++ javaXploreQuestions/javaQn1TravelAgencies.java
@@ -1,71 +1,15 @@
-public class javaQn1TravelAgencies {
 
-    // Declaring the class variables
-    int regNo;
-    String agencyName;
-    String packageType;
-    int price;
-    boolean FlightFacility;
+// Solution class which contains main method
 
-    // Constructor with parameters
-    public javaQn1TravelAgencies(int regNo, String agencyName, String packageType, int price, boolean FlightFacility) {
-        this.regNo=regNo;
-        this.agencyName=agencyName;
-        this.packageType=packageType;
-        this.price=price;
-        this.FlightFacility=FlightFacility;
+public class javaQn1TravelAgencies
+{
+    public static void main(String[] args) {
+       
+        TravelAgencies[] tAgencies = {
+            new TravelAgencies(1, "A2ZAgency", "Platinum", 50000, true),
+            new TravelAgencies(2, "SecondAgency", "Platinum", 60000, false),
+            new TravelAgencies(3, "ThirdAgency", "Platinum", 20000, true),
+            new TravelAgencies(4, "FourthAgency", "Platinum", 80000, false)
+        };
     }
-
-    //Making setters for each variables
-    public void setRegNo(int regNo)
-    {
-        this.regNo=regNo;
-    }
-
-    public void setAgencyName(String agencyName)
-    {
-        this.agencyName=agencyName;
-    }
-
-    public void setPackageType(String packageType)
-    {
-        this.packageType=packageType;
-    }
-
-    public void setPrice(int price)
-    {
-        this.price=price;
-    }
-
-    public void setFlightFacility(boolean FlightFacility)
-    {
-        this.FlightFacility=FlightFacility;
-    }
-
-    //Making getters for each variables
-    public int getRegNo()
-    {
-        return regNo;
-    }
-
-    public String getAgencyName()
-    {
-        return agencyName;
-    }
-
-    public String getPackageType()
-    {
-        return packageType;
-    }
-
-    public int getPrice()
-    {
-        return price;
-    }
-
-    public boolean getFlightFacility()
-    {
-        return FlightFacility;
-    }
-
 }
