d3f8b473dc82c55c6d6f7363e514b222a138754a
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-12-21 15:41:56 +0530

    making constructor

--- javaXploreQuestions/javaQn1TravelAgencies.java
+++ javaXploreQuestions/javaQn1TravelAgencies.java
@@ -1,8 +1,19 @@
 public class javaQn1TravelAgencies {
 
+    // Declaring the class variables
     int regNo;
     String agencyName;
     String packageType;
     int price;
     boolean FlightFacility;
+
+    // Constructor with parameters
+    public javaQn1TravelAgencies(int regNo, String agencyName, String packageType, int price, boolean FlightFacility) {
+        this.regNo=regNo;
+        this.agencyName=agencyName;
+        this.packageType=packageType;
+        this.price=price;
+        this.FlightFacility=FlightFacility;
+    }
+
 }
