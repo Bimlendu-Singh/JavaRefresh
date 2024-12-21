04646fc00b6569dfea6a975e1799eb86aaf86609
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-12-21 16:07:01 +0530

    Made method to find highest package from array of the objects

--- javaXploreQuestions/javaQn1TravelAgencies.java
+++ javaXploreQuestions/javaQn1TravelAgencies.java
@@ -11,5 +11,24 @@ public class javaQn1TravelAgencies
             new TravelAgencies(3, "ThirdAgency", "Platinum", 20000, true),
             new TravelAgencies(4, "FourthAgency", "Platinum", 80000, false)
         };
+
+        int highestPackagePrice = findAgencyWithHighestPackagePrice(tAgencies);
+        System.out.println(highestPackagePrice);
+    }
+
+    // Method to find highest package 
+    public static int findAgencyWithHighestPackagePrice(TravelAgencies[] obj)
+    {
+        int x=0;
+
+        for(TravelAgencies iterateA:obj)
+        {
+            if(x<iterateA.price)
+                x=iterateA.price;
+        }
+
+        return x;
+
     }
 }
+
