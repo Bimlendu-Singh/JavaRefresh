56342a009e6dd2c405f250d3d69b027659711f6e
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2024-12-21 15:50:51 +0530

    made getters and setters

--- javaXploreQuestions/javaQn1TravelAgencies.java
+++ javaXploreQuestions/javaQn1TravelAgencies.java
@@ -16,4 +16,56 @@ public class javaQn1TravelAgencies {
         this.FlightFacility=FlightFacility;
     }
 
+    //Making setters for each variables
+    public void setRegNo(int regNo)
+    {
+        this.regNo=regNo;
+    }
+
+    public void setAgencyName(String agencyName)
+    {
+        this.agencyName=agencyName;
+    }
+
+    public void setPackageType(String packageType)
+    {
+        this.packageType=packageType;
+    }
+
+    public void setPrice(int price)
+    {
+        this.price=price;
+    }
+
+    public void setFlightFacility(boolean FlightFacility)
+    {
+        this.FlightFacility=FlightFacility;
+    }
+
+    //Making getters for each variables
+    public int getRegNo()
+    {
+        return regNo;
+    }
+
+    public String getAgencyName()
+    {
+        return agencyName;
+    }
+
+    public String getPackageType()
+    {
+        return packageType;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public boolean getFlightFacility()
+    {
+        return FlightFacility;
+    }
+
 }
