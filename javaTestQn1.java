f6236cb03e02a2c8b4c1bca1dcb0b83017ae1590
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2025-01-17 21:56:47 +0530

    added javaIgnite test question program

--- javaTestQn1.java
+++ javaTestQn1.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class javaTestQn1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count=0,a,b,x,y;
+
+        a=sc.nextInt();
+        b=sc.nextInt();
+
+        x=a;
+        y=b;
+
+        x=x/10;
+        if(x==0||x>9)
+            count++;
+
+        y=y/10;
+        if(y==0||y>9)
+            count++;
+
+        if(count==0)
+        {
+            System.out.println("Two digit number");
+        }
+        else{
+            System.out.println("Invalid Input!");
+        }
         
+        
+
     }
 }
