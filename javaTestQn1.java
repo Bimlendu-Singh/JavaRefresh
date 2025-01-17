890d49d8c10a0fb59e9252046937174fe24d5c6c
BheemSingh <bimlendukumarsingh.bks4@gmail.com>
2025-01-17 21:59:36 +0530

    added javaIgnite test question program

--- javaTestQn1.java
+++ javaTestQn1.java
@@ -5,23 +5,28 @@ public class javaTestQn1 {
         Scanner sc = new Scanner(System.in);
         int count=0,a,b,x,y;
 
+        // User input 
         a=sc.nextInt();
         b=sc.nextInt();
 
+        // Assigning a and b to x and y so data doesn't loss
         x=a;
         y=b;
 
+        // If a or x is not a two digit number count will be increased by 1
         x=x/10;
         if(x==0||x>9)
             count++;
 
+        // If b or y is not a two digit number count will be increased by 1
         y=y/10;
         if(y==0||y>9)
             count++;
 
+        //Only will enter if block when both entered number is two digit number..
         if(count==0)
         {
-            System.out.println("Two digit number");
+           
         }
         else{
             System.out.println("Invalid Input!");
